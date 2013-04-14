63309f6314993d84ec6ab8a4e1b45d6693947f7f
John Fawcett <jrf0110@gmail.com>
2013-04-14 03:20:04 -0500

    WIP

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var claude = require('claude');
 
-claud.init({
-  clientId:     process.env.GITHUB_CLIENT_ID
-, clientSecret: process.env.GITHUB_CLIENT_SECRET
-, appsDir:      '/apps'
-, port:         3001
+claude.init({
+  github: {               // github
+    clientId:             process.env.GITHUB_CLIENT_ID
+  , clientSecret:         process.env.GITHUB_CLIENT_SECRET
+  , username:             'jrf0110'
+  }
+
+, nginx: {                // nginx
+    configFile:           '/usr/local/etc/nginx/nginx.conf'
+  , includeDetection:     'include       mime.types;'
+  , tab:                  '    '
+  , restart:              'nginx -s stop && nginx'
+  // , restart:              ['nginx ; nginx', '-s', 'stop']
+  }
+
+                          // misc
+, appDir:                 '/apps'
+, dataPath:               process.cwd() + '/data.js'
+, port:                   3001
 });
